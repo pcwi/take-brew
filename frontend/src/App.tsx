53cbed47286c8116e2bb00cd04ab41d53a522e5b
pcwi <pikerm@gmail.com>
2022-03-13 13:54:17 +0100

    Added bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Container, Navbar, NavbarBrand} from 'reactstrap'
 
 function App() {
   const apiUrl = process.env.REACT_APP_API_URL!!
@@ -15,28 +16,43 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-         Response from {process.env.REACT_APP_API_URL}
-        </p>
-        <p>
-         {message}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+<>
+<Navbar
+    color="light"
+    container="md"
+    expand="md"
+    light
+  >
+    <NavbarBrand href="/">
+    <h1><img src="logo.svg" width="120" height="120"/>
+      Little Hills Home Brewery</h1>
+    </NavbarBrand>
+    </Navbar>
+
+
+    <Container className='container-md'
+    >
+      <Card>
+      <CardBody>
+        <CardTitle tag="h5">
+          Info from Backend
+        </CardTitle>
+        <CardSubtitle
+          className="mb-2 text-muted"
+          tag="h6"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Response from {process.env.REACT_APP_API_URL}
+        </CardSubtitle>
+        <CardText>
+          {message}
+        </CardText>
+        <Button>
+          Gimme
+        </Button>
+      </CardBody>
+      </Card>
+    </Container>
+    </>
   );
 }
 
